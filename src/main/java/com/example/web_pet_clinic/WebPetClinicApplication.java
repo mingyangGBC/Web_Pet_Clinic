249ec99ffbe40880b3b506d7aed56827faa93c5b
Ming Yang <ming.yang@georgebrown.ca>
2021-10-28 18:34:07 -0400

    first commit gitignore

---
+++ src/main/java/com/example/web_pet_clinic/WebPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_pet_clinic/WebPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
